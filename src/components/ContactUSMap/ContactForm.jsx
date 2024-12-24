3cc090ac856d9f986db1e26af9b45fcb0ce08bf7
NEEL-KALATHIYA <kalathiyaneel4@gmail.com>
2024-12-24 12:40:16 +0530

    email

--- src/components/ContactUSMap/ContactForm.jsx
+++ src/components/ContactUSMap/ContactForm.jsx
@@ -11,7 +11,7 @@ import {
 import { useForm, ValidationError } from "@formspree/react";
 
 const ContactForm = () => {
-  const [state, handleSubmit] = useForm("xpwwzlqr");
+  const [state, handleSubmit] = useForm("xeooqrpk");
 
   const [formData, setFormData] = useState({
     name: "",
