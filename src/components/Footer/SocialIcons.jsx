1f497a6ec6c29ed81f81ec929ddf94f60065cf39
Param-Kumar Jadvani <jadvaniparam@gmail.com>
2024-12-25 15:53:56 +0530

    Update url of icon

--- src/components/Footer/SocialIcons.jsx
+++ src/components/Footer/SocialIcons.jsx
@@ -6,18 +6,49 @@ import {
   FaTwitterSquare,
   FaLinkedin,
   FaInstagramSquare,
+  FaYoutubeSquare,
 } from "react-icons/fa";
 
 const SocialIcons = () => {
   const theme = useTheme(); // Access theme for consistent styling
 
-  // Shared IconButton styles using theme
-  const iconButtonStyles = {
-    color: theme.palette.common.white, // Use theme's white color
-    "&:hover": {
-      color: theme.headerTextColor, // Use theme's secondary color for hover
-    },
+  // Define hover colors for each icon
+  const hoverColors = {
+    twitter: "#1DA1F2", // Twitter blue
+    linkedin: "#0077b5", // LinkedIn blue
+    instagram: "#E1306C", // Instagram pink
+    facebook: "#1877F2", // Facebook blue
+    youtube: "#FF0000", // YouTube red
   };
+
+  // Social links and their associated hover colors and icons
+  const socialLinks = [
+    {
+      href: "https://x.com/vymanika_aero",
+      icon: FaTwitterSquare,
+      hoverColor: hoverColors.twitter,
+    },
+    {
+      href: "https://www.linkedin.com/company/vymanika-next-gen-aerospace-technologies/",
+      icon: FaLinkedin,
+      hoverColor: hoverColors.linkedin,
+    },
+    {
+      href: "https://www.instagram.com/vymanika_aerospace/",
+      icon: FaInstagramSquare,
+      hoverColor: hoverColors.instagram,
+    },
+    {
+      href: "https://www.facebook.com/profile.php?id=100093604593799&mibextid=ZbWKwL",
+      icon: FaFacebookSquare,
+      hoverColor: hoverColors.facebook,
+    },
+    {
+      href: "https://www.youtube.com/@vymanikaaerospace",
+      icon: FaYoutubeSquare,
+      hoverColor: hoverColors.youtube,
+    },
+  ];
 
   return (
     <Box
@@ -29,21 +60,23 @@ const SocialIcons = () => {
         marginTop: 2,
       }}
     >
-      <IconButton component="a" href="#" sx={iconButtonStyles}>
-        <FaTwitterSquare size={24} />
-      </IconButton>
-
-      <IconButton component="a" href="#" sx={iconButtonStyles}>
-        <FaLinkedin size={24} />
-      </IconButton>
-
-      <IconButton component="a" href="#" sx={iconButtonStyles}>
-        <FaInstagramSquare size={24} />
-      </IconButton>
-
-      <IconButton component="a" href="#" sx={iconButtonStyles}>
-        <FaFacebookSquare size={24} />
-      </IconButton>
+      {socialLinks.map(({ href, icon: Icon, hoverColor }) => (
+        <IconButton
+          key={href}
+          component="a"
+          href={href}
+          target="_blank" // Opens link in a new tab
+          rel="noopener noreferrer" // Security best practice
+          sx={{
+            color: theme.palette.common.white,
+            "&:hover": {
+              color: hoverColor,
+            },
+          }}
+        >
+          <Icon size={22} />
+        </IconButton>
+      ))}
     </Box>
   );
 };
